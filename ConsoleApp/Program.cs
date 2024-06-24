eab11b79c434002a77b34ad1a1ce0fe455c331e7
Lilia Shamsutdinova <Lilia.Shamsutdinova@jetbrains.com>
2024-06-24 15:58:43 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void Test()
+{
+    Console.WriteLine("Test");
+}
+
+internal record User(string FirstName, string LastName, int Age, int NumberOfCats)
+{
+    class MyClass
+    {
+        /*string*/ void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
